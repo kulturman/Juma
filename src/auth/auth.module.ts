52f0b692b3b17640d4f6d3ff43a38f925ad420d0
kulturman <arnaudbakyono@gmail.com>
2022-05-26 18:50:16 +0000

    Add useContainer to be able to inject things in Validators

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { IsSameConstraint } from "src/helpers/validation/IsSame";
 import { AuthService } from "./auth.service";
 import { RegisterController } from "./register.controller";
 import { UserRepository } from "./repositories/user.repository";
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,7 +6,7 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(private userRepository: UserRepository) {}
+    constructor(public userRepository: UserRepository) {}
 
     async register(registerDto: RegisterDto) {
         const user = new User();
