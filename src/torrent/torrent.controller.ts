ec9d3f65ae64f29f0e8315d6d87fabee4e9c4703
kulturman <arnaudbakyono@gmail.com>
2022-05-31 22:54:27 +0000

    Remove uploads directory in src

--- src/torrent/torrent.controller.ts
+++ src/torrent/torrent.controller.ts
@@ -2,6 +2,7 @@ import { InjectQueue } from "@nestjs/bull";
 import { Controller, Post, Request, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { Queue } from "bull";
+import { join } from "path";
 import { TorrentService } from "./torrent.service";
 
 @Controller('torrents')
@@ -12,10 +13,10 @@ export class TorrentController {
     ) {}
 
     @Post()
-    @UseInterceptors(FileInterceptor('file', {dest: 'uploads/torrents'}))
+    @UseInterceptors(FileInterceptor('file', {dest: join(__dirname, '../uploads/torrents')}))
     async uploadFile(@UploadedFile() file: Express.Multer.File, @Request() req) {
         const torrent = await this.torrentService.createNewTorrent(file.path, req.user.id);
-
+        
         const job = await this.downloadTorrentQueue.add({
             'torrentId': torrent.id,
         });
--- src/torrent/torrent.module.ts
+++ src/torrent/torrent.module.ts
@@ -1,7 +1,6 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 import { AuthModule } from 'src/auth/auth.module';
 import { DownloadTorrentProcessor } from './download-torrent.processor';
 import { Torrent } from './entities/torrent.entity';
@@ -14,7 +13,6 @@ import { TorrentService } from './torrent.service';
         AuthModule,
         BullModule.registerQueue({
             name: 'downloadTorrent',
-            //processors: [join(__dirname, 'download-torrent.processor.js')]
         })
     ],
     controllers: [TorrentController],
