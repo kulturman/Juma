91a4fbcd5526b400acb3b53168908aa159e8d9fd
kulturman <arnaudbakyono@gmail.com>
2022-06-06 23:50:57 +0000

    Remove i18n module

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,18 +20,7 @@ import { BullModule } from '@nestjs/bull';
         host: 'localhost',
         port: 6379
       }
-    }),
-    I18nModule.forRoot({
-      fallbackLanguage: 'en',
-      loaderOptions: {
-        path: join(__dirname, '/i18n/'),
-        watch: true,
-      },
-      resolvers: [
-        { use: QueryResolver, options: ['lang'] },
-        AcceptLanguageResolver,
-      ],
-    }),
+    })
   ],
   providers: [],
 })
