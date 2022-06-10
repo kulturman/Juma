97d3ac13e708f282c295612767f12e38cbd23cd0
kulturman <arnaudbakyono@gmail.com>
2022-06-10 23:00:46 +0000

    Prevent directories traversal

--- src/file-explorer/file-explorer.controller.ts
+++ src/file-explorer/file-explorer.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Get, NotFoundException, Param, Req } from "@nestjs/common";
+import { Controller, ForbiddenException, Get, NotFoundException, Param, Req } from "@nestjs/common";
 import { join } from "path";
 import { FileExplorerService } from "./file-explorer.service";
-import * as fs from 'fs';
-
 
 @Controller('fs')
 export class FileExplorerController {
     constructor(private readonly fileExplorerService: FileExplorerService) {}
 
     @Get('folder/:folderPath?')
-    getFolderContent(@Req() req, @Param('folderPath') folderPath) {
+    getFolderContent(@Req() req, @Param('folderPath') folderPath: string) {
         const basePath = join(__dirname, `../${req.user.id}`);
 
+        if (folderPath && folderPath.includes('..')) {
+            throw new ForbiddenException('Nice try, cannot let you explore directories like that');
+        }
         return this.fileExplorerService.getFolderContent(basePath, folderPath || '');
     }
 }
