e33144ecc82dbf875ee34cd71995ceee7d843127
kulturman <arnaudbakyono@gmail.com>
2022-06-11 13:03:06 +0000

    Remove unused import

--- src/file-explorer/file-explorer.module.ts
+++ src/file-explorer/file-explorer.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { FileExplorerController } from './file-explorer.controller';
 import { FileExplorerService } from './file-explorer.service';
 
