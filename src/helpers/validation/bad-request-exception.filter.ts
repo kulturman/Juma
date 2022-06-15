cdbcce2c5d349c5661f0cca855a6e15160b752c3
kulturman <arnaudbakyono@gmail.com>
2022-06-15 18:29:25 +0000

    Change BadRequestExceptionRequest Format

---
+++ src/helpers/validation/bad-request-exception.filter.ts
@@ -0,0 +1,29 @@
+import { ExceptionFilter, Catch, ArgumentsHost, BadRequestException, ValidationError } from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(BadRequestException)
+export class BadRequestExceptionFilter implements ExceptionFilter {
+  catch(exception: BadRequestException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    const errors = <{ message: [ValidationError] }>exception.getResponse();
+    
+    const data = errors.message.map(error => {
+      return {
+        field: error.property,
+        constraints: Object.keys(error.constraints).map((key, index) => {
+          return {
+            constraint: key,
+            message: error.constraints[key]
+          };
+        }),
+        value: error.value
+      }
+    })
+
+    response
+      .status(status)
+      .json(data);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { BadRequestExceptionFilter } from './helpers/validation/bad-request-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(
     new ValidationPipe({
+      exceptionFactory: (errors) => new BadRequestException(errors),
       transform: true
     }),
   );
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new BadRequestExceptionFilter())
   await app.listen(process.env.PORT);
 }
 bootstrap();
