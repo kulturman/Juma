81a5caac0d9a90ad4c7b8a5512a9d152aef168c0
kulturman <arnaudbakyono@gmail.com>
2022-06-21 00:22:20 +0000

    Fix silly typo

--- src/file-explorer/directory-traversal-preventing.middleware.ts
+++ src/file-explorer/directory-traversal-preventing.middleware.ts
@@ -6,7 +6,7 @@ export function preventDirectoryTraversalMiddleware(
   res: Response,
   next: NextFunction,
 ) {
-  //Since parameters are not parsed yet I just check the URL for .. (directory travarsal)
+  //Since parameters are not parsed yet I just check the URL for .. (directory traversal)
   if (req.originalUrl.includes('..')) {
     throw new ForbiddenException(
       'Nice try, cannot let you explore directories like that',
