390503813fd9baada9b10b632676ed23f5653909
kulturman <arnaudbakyono@gmail.com>
2022-06-28 23:50:52 +0000

    Add axios and configure base url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:5000/api/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
