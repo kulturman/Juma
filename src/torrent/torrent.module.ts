e289c3832857a1cd5e5d6880395adeac30dc0642
kulturman <arnaudbakyono@gmail.com>
2023-04-08 01:53:00 +0000

    Fiw broken imports

--- src/torrent/torrent.module.ts
+++ src/torrent/torrent.module.ts
@@ -1,7 +1,7 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/auth/entities/user.entity';
+import { User } from '../auth/entities/user.entity';
 import { AuthModule } from '../auth/auth.module';
 import { DownloadTorrentProcessor } from './download-torrent.processor';
 import { Torrent } from './entities/torrent.entity';
--- src/torrent/torrent.service.ts
+++ src/torrent/torrent.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/auth/entities/user.entity';
+import { User } from '../auth/entities/user.entity';
 import { Repository } from 'typeorm';
 import { Torrent } from './entities/torrent.entity';
 
